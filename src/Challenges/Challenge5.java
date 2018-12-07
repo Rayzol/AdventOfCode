f3a874ce8298d64289a20af8e16b37a629c9a3b0
Gary Malone <gmalone@uk.ibm.com>
2018-12-07 11:41:29 +0000

    day 5 part 2 finished

--- src/Challenges/Challenge5.java
+++ src/Challenges/Challenge5.java
@@ -15,33 +15,57 @@ public class Challenge5 extends Challenge {
 	}
 	@Override
 	void partOne() {
-		
 		for(String s : this.input){
-			for(int i = 0; i < s.length(); i++) {
-				
-				//is the current and next character the same letter but different cases?
-				if(i + 2 < s.length() && s.substring(i, i + 1).toLowerCase().equals(s.substring(i + 1, i + 2).toLowerCase()) &&
-						!s.substring(i, i + 1).equals(s.substring(i + 1, i + 2))) {
-					
-					String substring = s.substring(i, i + 1) + s.substring(i + 1, i + 2);
-					s = s.replace(substring, "");
-					
-					//set the iterator to the start
-					i = -1; 
-					System.out.print("The length string is: " + s.length() +"\n");
-
-				}
-			}
-			
-			System.out.print("The length of the finished string is: " + s.length());
+			s = reductStringPolymer(s);
+			System.out.print("The length of the finished string is: " + s.length() + "\n");
 		}
-
 	}
 
 	@Override
 	void partTwo() {
-		// TODO Auto-generated method stub
-
+		for(String s : this.input){
+			s = reductStringPolymer(s);
+			int maxLength = s.length();
+			
+			//iterate through A-Z
+			for(int i = 65; i <= 90; i++) {
+				String newString = s;
+				String characterToRemove = String.valueOf((char) i);
+				newString = removeCharacter(newString, characterToRemove);
+				newString = reductStringPolymer(newString);
+				if(maxLength > newString.length()) {
+					maxLength = newString.length();
+				}
+			}
+			
+			System.out.print("The length of the shortest string is: " + maxLength);
+		}
+	}
+	
+	public String reductStringPolymer(String s) {
+		for(int i = 0; i < s.length(); i++) {
+			
+			//is the current and next character the same letter but different cases?
+			if(i + 2 < s.length() && s.substring(i, i + 1).toLowerCase().equals(s.substring(i + 1, i + 2).toLowerCase()) &&
+					!s.substring(i, i + 1).equals(s.substring(i + 1, i + 2))) {
+				
+				String substring = s.substring(i, i + 1) + s.substring(i + 1, i + 2);
+				s = s.replace(substring, "");
+				
+				//set the iterator
+				if(i - 2 < -1) {
+					i = -1; 
+				}
+				else {
+					i = i -2;
+				}
+			}
+		}
+		return s;
 	}
 
+	public String removeCharacter(String s, String characterToRemove) {
+		s = s.replaceAll("["+characterToRemove.toLowerCase() + characterToRemove.toUpperCase()+"]", "");
+		return s;
+	}
 }
